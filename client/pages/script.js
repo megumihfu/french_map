44371bebef1bf6cdd2c51eed51d8cbff5c3cc020
Inès DAFFEF <daffef@et.esiea.fr>
2024-02-11 16:57:43 +0100

    ajout du targets + modif

--- client/pages/script.js
+++ client/pages/script.js
@@ -4,52 +4,46 @@ function convertCoordinates(longitude, latitude, width, height) {
     return { x, y };
 }
 
-const width = 1400;
-const height = 1200;
+const width = 995;
+const height = 960;
 
-const svg = d3.select("#svg2")
+const svg = d3.select("#svg3811")
     .attr("width", width)
     .attr("height", height);
 
 fetch('http://localhost:8080/cities')
     .then(response => response.json())
     .then(data => {
-        console.log('Données récupérées avec succès :', data);
+        console.log('Données récupérées avec succes :', data);
 
-    let minLongitude = Number.MAX_VALUE;
-    let maxLongitude = Number.MIN_VALUE;
-    let minLatitude = Number.MAX_VALUE;
-    let maxLatitude = Number.MIN_VALUE;
 
-    data.forEach(city => {
-        minLongitude = Math.min(minLongitude, city.longitude);
-        maxLongitude = Math.max(maxLongitude, city.longitude);
-        minLatitude = Math.min(minLatitude, city.latitude);
-        maxLatitude = Math.max(maxLatitude, city.latitude);
-    });
+        console.log("buenos dias")
+        const mainCities = ["Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Montpellier", "Strasbourg", "Bordeaux", "Lille", "Rennes", "Reims", "Toulon", "Grenoble", "Dijon", "Angers"];
+        const displayCities = data.filter(city => mainCities.includes(city.name));
+        console.log(displayCities);
 
-    console.log('minLongitude:', minLongitude);
-    console.log('maxLongitude:', maxLongitude);
-    console.log('minLatitude:', minLatitude);
-    console.log('maxLatitude:', maxLatitude);
+        let minLongitude = -5; 
+        let maxLongitude = 10; 
+        let minLatitude = 41; 
+        let maxLatitude = 51;
+        
+        const xScale = d3.scaleLinear()
+            .domain([minLongitude, maxLongitude])
+            .range([49, width]);
+        
+        const yScale = d3.scaleLinear()
+            .domain([minLatitude, maxLatitude])
+            .range([height, 57]);
 
-    const xScale = d3.scaleLinear()
-    .domain([minLongitude, maxLongitude])
-    .range([0, width]);
-
-    const yScale = d3.scaleLinear()
-        .domain([minLatitude, maxLatitude])
-        .range([height, 0]);
-
-    svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xScale(d.longitude))
-        .attr("cy", d => yScale(d.latitude))
-        .attr("r", 5)
-        .attr("fill", "red")
-        .append("title")
-        .text(d => d.nom);
-    })
-    .catch(error => console.error('Erreur lors de la récupération des données :', error));
+        svg.selectAll("circle")
+            .data(displayCities)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xScale(d.longitude))
+            .attr("cy", d => yScale(d.latitude))
+            .attr("r", 7)
+            .attr("fill", "red")
+            .append("title")
+            .text(d => d.nom);
+    
+})
